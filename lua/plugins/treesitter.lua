f72041df26cfdc396c6991e6a8b6b5b2ce717a14
Alexandr133123 <gagahre4@gmail.com>
2025-06-16 22:03:43 +0300

    added treesitter ensure_installed setup option

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,6 +5,20 @@ return {
 		config = function()
 			require("nvim-treesitter.install")
 			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"sql",
+					"css",
+					"go",
+					"c_sharp",
+					"html",
+					"javascript",
+					"json",
+					"lua",
+					"markdown",
+					"typescript",
+					"dockerfile",
+				},
+				auto_install = true,
 				highlight = {
 					enable = true,
 				},
